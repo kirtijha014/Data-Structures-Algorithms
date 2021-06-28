690af3cb585b9adcd7e9c9436dc351f43a9c78ae
kirtijha014 <72241737+kirtijha014@users.noreply.github.com>
2021-06-28 10:38:53 +0530

    Add files via upload

---
+++ Sorting and Searching/BubbleSort_Optimized.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+void bubble_sort(int arr[], int n)
+{
+	for (int i = 0; i< n-1; i++)
+	{
+		bool swapped;
+		for(int j = 0; j < n-1; j++)
+		{
+			swapped = false;
+			if(arr[j]>arr[j+1])
+			{
+				swap(&arr[j],&arr[j+1]);
+				swapped = true;
+			}
+		}
+		if(swapped ==  false)
+		break;
+	}
+}
+int main()
+{
+	int n,i;
+	cout<<"Enter size of the array: ";
+	cin>>n;
+	int *a = new int[n];
+	cout << "\nEnter array elements: ";
+	for(i =  0; i < n; i++)
+	cin>>a[i];
+	bubble_sort(a,n);
+	cout << "\nThe sorted array is :";
+	for(i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+	delete []a;
+	return 0;
+}
