65488ebcfd0b3f8fe038759d3e127a61f5ce9aa4
kirtijha014 <72241737+kirtijha014@users.noreply.github.com>
2021-07-03 18:15:44 +0530

    Create MergeSort_Complexity.cpp

---
+++ Time Complexity/MergeSort_Complexity.cpp
@@ -0,0 +1,84 @@
+#include<sys/time.h>
+#include<iostream>
+
+using namespace std;
+long getTimeinMicroseconds()
+{
+	struct timeval start;
+	gettimeofday(&start, NULL);
+	return start.tv_sec * 1000000 + start.tv_usec;
+}
+
+void merge(int arr[] ,int start, int mid, int end)
+{
+	int temp[end-start];
+	int i = start;
+	int j = mid+1;
+	int k = 0;
+	while(i <= mid && j <= end)
+	{
+		if(arr[i] < arr[j])
+		{
+			temp[k++] = arr[i++];
+		}
+		else
+		{
+			temp[k++] = arr[j++];
+		}
+	}
+	if(i <= mid)
+	{
+		while(i <=mid)
+		{
+			temp[k++] = arr[i++];
+		}
+	}
+	else if (j <= end)
+	{
+		while(j <= end)
+		{
+			temp[k++] = arr[j++];
+		}
+	}
+	k = 0;
+	for(int l = start; l <=end; l++)
+	{
+		arr[l] = temp[k++];
+	}
+}
+ 
+void mergesort(int arr[], int start, int end)
+{
+	if(start >= end)
+	{
+		return;
+	}
+	int mid = (end+start)/2;
+	mergesort(arr, start, mid);
+	mergesort(arr, mid+1,end);
+	merge(arr,start,mid,end);
+}
+int main()
+{
+	for(int n = 10; n <= 1000000; n*= 10)
+	{
+		int *arr = new int[n];
+		long starttime, endtime;
+		for(int i = 0; i < n; i++)
+		{
+			arr[i] = n-i;
+		}
+		starttime = getTimeinMicroseconds();
+		mergesort(arr,0,n-1);
+		endtime = getTimeinMicroseconds();
+		cout << "Merge Sort n = " << n << "time = " << endtime-starttime << endl;
+	}
+	
+	return 0;
+}
+/*Time Complexity for Merge Sort  Output:
+	Merge Sort n = 10time = 0
+	Merge Sort n = 100time = 0
+	Merge Sort n = 1000time = 0
+	Merge Sort n = 10000time = 0
+	Merge Sort n = 100000time = 5000*/
