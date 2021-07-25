bb579e26154d70af1843f9500a5654de417a3e23
kirtijha014 <72241737+kirtijha014@users.noreply.github.com>
2021-07-25 22:22:44 +0530

    Update CopyConstructor.cpp

--- OOPs/CopyConstructor.cpp
+++ OOPs/CopyConstructor.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	Student s;                        //Default Constructor
-	Student s1(101);					// Constructor 2
+	Student s1(101);	         // Constructor 2
 	cout << "S1 : ";
 	s1.setAge(20);					
 	s1.display();
@@ -14,7 +14,7 @@ int main()
 	s2.setAge(21);
 	s2.display();
 	
-	Student *s3 = new Student(s2);         //Copy Constructor
+	Student *s3 = new Student(s2);   //Copy Constructor
 	cout << "\nS3 : ";
 	s3->setAge(25);
 	s3->display();
@@ -24,11 +24,11 @@ int main()
 	s4.setAge(29);
 	s4.display();
 	
-	s4 = s1;                       // No constructor called as already s4 and s1 are created
+	s4 = s1;                        // No constructor called as already s4 and s1 are created
 	cout << "\nS4 : ";
 	s4.display();
 	cout << "\nS5 : ";
-	Student s5 = s4;				//Copy Constructor
+	Student s5 = s4;		//Copy Constructor
 	s5.display();
 	//using this delete keyword because s3 is a dynamically created object. Hence it will help destructor to identify s3 object and deallocate it.
 	delete s3;
