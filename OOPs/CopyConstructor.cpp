348a80592118d3df2c439a711ad7e8572c9f7a69
kirtijha014 <72241737+kirtijha014@users.noreply.github.com>
2021-07-25 22:12:32 +0530

    Add files via upload

--- OOPs/CopyConstructor.cpp
+++ OOPs/CopyConstructor.cpp
@@ -4,8 +4,30 @@ using namespace std;
 
 int main()
 {
+	Student s;
 	Student s1(101);
+	cout << "S1 : ";
+	s1.setAge(20);
+	s1.display();
 	Student s2(s1);
+	cout << "\nsS2 : ";
+	s2.setAge(21);
 	s2.display();
+	
+	Student *s3 = new Student(s2);
+	cout << "\nS3 : ";
+	s3->setAge(25);
+	s3->display();
+	
+	Student s4(*s3);
+	cout << "\nS4 : ";
+	s4.setAge(29);
+	s4.display();
+	
+	s4 = s1;
+	cout << "\nS4 : ";
+	s4.display();
+	//using this delete keyword because s3 is a dynamically created object. Hence it will help destructor to identify s3 object and deallocate it.
+	delete s3;
 	return 0;
 }
