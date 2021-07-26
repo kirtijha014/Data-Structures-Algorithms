3a08af346250fc58be7d7d96cd955c39a1523211
kirtijha014 <72241737+kirtijha014@users.noreply.github.com>
2021-07-26 10:19:14 +0530

    Update Shallow and Deep Copy.cpp

--- OOPs/Shallow and Deep Copy.cpp
+++ OOPs/Shallow and Deep Copy.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
-#include "StudentClassWithName.cpp"
 
+class Student{
+	int age;
+	char *name;
+	public:
+		Student(int age,char*name)
+		{
+			this->age = age;
+			//Shallow Copy: Copying the address of 0th element and not the whole array
+			//this->name = name;
+			
+			
+			
+			//Deep Copy: Creating a new array yto copy the whole array
+			this->name = new char[strlen(name)+1];
+			strcpy(this->name,name);
+		}
+		
+		
+		void display()
+		{
+			cout<< "Name: "<< name <<" Age: "<< age <<endl;
+		}
+};
 int main()
 {
 	char name[] = "Kirti";
