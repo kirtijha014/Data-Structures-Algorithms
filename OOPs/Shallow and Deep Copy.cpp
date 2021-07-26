8992b33b9471a24e658516fa4d2f82253a4de61a
kirtijha014 <72241737+kirtijha014@users.noreply.github.com>
2021-07-26 10:04:30 +0530

    Add files via upload

---
+++ OOPs/Shallow and Deep Copy.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include "StudentClassWithName.cpp"
+
+int main()
+{
+	char name[] = "Kirti";
+	Student s1(20,name);
+	s1.display();
+	
+	name[3] = 'e';
+	Student s2(24,name);
+	s2.display();
+	s1.display();
+	return 0;
+}
