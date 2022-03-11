41b5a37dafe1339e590dbd225ed204b97b378e62
Kirti <kirtijha014@gmail.com>
2022-03-11 11:53:21 +0530

    Sqaure root with precision using Binary Search

--- Binary Search/SquareRootUsingBS.cpp
+++ Binary Search/SquareRootUsingBS.cpp
@@ -1,28 +1,49 @@
 #include<iostream>
 using namespace std;
 
-int BinarySearch(int target)
+int sqrtInteger(int target)
 {
     int start = 0, end = target;
     long long int ans = -1;
     while(start <= end)
     {
-        
+        long long int mid = (start + end) >> 1;
+        long  long int square = mid * mid;
+
+        if(target == square)
+        return mid;
+        else if(square < target)
+        {
+            ans = mid;
+            start = mid + 1;
+        }        
+        else
+        end = mid - 1;
     }
+    return ans;
 }
 
-
-
-
-int NthRoot(int target)
+double morePrecision(int target, int precision, int tempSolution)
 {
-
+    double factor = 1;
+    double ans = tempSolution;
+    for(int i = 0; i < precision; i++)
+    {
+        factor = factor / 10;
+        for(double j = ans; j * j < target; j= j + factor)
+        {
+            ans = j;
+        }
+    }
+    return ans;
 }
+
 
 
 int main()
 {
     int target;
     cin >> target;
-    cout << NthRoot(target);
+    int tempSol = sqrtInteger(target);
+    cout << "Answer is : " << morePrecision(target, 3, tempSol);
 }
